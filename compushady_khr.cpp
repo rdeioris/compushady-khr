449f98dd62223ddd9d926b44c4e9873378e6ce08
rdeioris <roberto.deioris@gmail.com>
2024-07-22 11:57:51 +0200

    improved glsl compiler

--- compushady_khr.cpp
+++ compushady_khr.cpp
@@ -15,61 +15,107 @@
 
 extern "C"
 {
-    COMPUSHADY_KHR_API const uint8_t *compushady_khr_glsl_to_spv(const char *glsl, const size_t glsl_size, void *(*allocator)(const size_t), size_t *spv_size)
+    COMPUSHADY_KHR_API void *compushady_khr_malloc(const size_t size)
+    {
+        return malloc(size);
+    }
+
+    COMPUSHADY_KHR_API void compushady_khr_free(void *ptr)
+    {
+        free(ptr);
+    }
+
+    COMPUSHADY_KHR_API const uint8_t *compushady_khr_glsl_to_spv(const char *glsl, const size_t glsl_size, const char *shader_model, const uint32_t flags, size_t *spv_size, char **error_ptr, size_t *error_len, void *(*allocator)(const size_t))
     {
         uint8_t *spv = nullptr;
         *spv_size = 0;
+        *error_ptr = nullptr;
+        *error_len = 0;
 
         if (!allocator)
         {
-            allocator = malloc;
+            allocator = compushady_khr_malloc;
         }
 
         glslang::InitializeProcess();
 
         {
+            EShLanguage language = EShLanguage::EShLangCompute;
+            if (shader_model && ::strlen(shader_model) >= 2)
+            {
+                if (shader_model[0] == 'c' && shader_model[0] == 's')
+                {
+                    language = EShLanguage::EShLangCompute;
+                }
+                else if (shader_model[0] == 'v' && shader_model[0] == 's')
+                {
+                    language = EShLanguage::EShLangVertex;
+                }
+                else if (shader_model[0] == 'p' && shader_model[0] == 's')
+                {
+                    language = EShLanguage::EShLangFragment;
+                }
+                else if (shader_model[0] == 'm' && shader_model[0] == 's')
+                {
+                    language = EShLanguage::EShLangMesh;
+                }
+            }
 
             glslang::TProgram program;
 
-            glslang::TShader shader(EShLanguage::EShLangCompute);
+            glslang::TShader shader(language);
 
             const int glsl_size_int = static_cast<int>(glsl_size);
 
             shader.setStringsWithLengths(&glsl, &glsl_size_int, 1);
             shader.setEntryPoint("main");
-            shader.setEnvInput(glslang::EShSource::EShSourceGlsl, EShLanguage::EShLangCompute, glslang::EShClient::EShClientVulkan, 100);
+            shader.setEnvInput(glslang::EShSource::EShSourceGlsl, language, glslang::EShClient::EShClientVulkan, 100);
             shader.setEnvClient(glslang::EShClient::EShClientVulkan, glslang::EshTargetClientVersion::EShTargetVulkan_1_0);
             shader.setEnvTarget(glslang::EShTargetLanguage::EShTargetSpv, glslang::EShTargetLanguageVersion::EShTargetSpv_1_0);
 
+            if (flags & 1)
+            {
+                shader.setShiftBinding(glslang::TResourceType::EResUbo, 0);
+                shader.setShiftBinding(glslang::TResourceType::EResImage, 1024);
+                shader.setShiftBinding(glslang::TResourceType::EResTexture, 1024);
+                shader.setShiftBinding(glslang::TResourceType::EResUav, 2048);
+                shader.setShiftBinding(glslang::TResourceType::EResSsbo, 2048);
+                shader.setShiftBinding(glslang::TResourceType::EResSampler, 3072);
+            }
+
             if (!shader.parse(GetDefaultResources(), 100, false, EShMsgDefault))
             {
-                printf("AAAAAAA! %s\n", shader.getInfoLog());
+                const char *info_log = shader.getInfoLog();
+                *error_len = ::strlen(info_log);
+                *error_ptr = reinterpret_cast<char *>(allocator(*error_len));
+                ::memcpy(*error_ptr, info_log, *error_len);
+                glslang::FinalizeProcess();
+                return nullptr;
             }
 
             program.addShader(&shader);
 
-            printf("OK\n");
-
             if (!program.link(EShMsgDefault))
             {
-                printf("OOPS\n");
+                const char *info_log = program.getInfoLog();
+                *error_len = ::strlen(info_log);
+                *error_ptr = reinterpret_cast<char *>(allocator(*error_len));
+                ::memcpy(*error_ptr, info_log, *error_len);
+                glslang::FinalizeProcess();
+                return nullptr;
             }
 
             if (!program.mapIO())
             {
-                printf("OOPS2\n");
+                const char *info_log = program.getInfoLog();
+                *error_len = ::strlen(info_log);
+                *error_ptr = reinterpret_cast<char *>(allocator(*error_len));
+                ::memcpy(*error_ptr, info_log, *error_len);
+                glslang::FinalizeProcess();
+                return nullptr;
             }
 
-            printf("OK2\n");
-
-            printf("InfoLog: %s\n", program.getInfoLog());
-            printf("InfoDebugLog: %s\n", program.getInfoDebugLog());
-
-            // program.buildReflection(EShReflectionDefault);
-
             glslang::TIntermediate *intermediate = program.getIntermediate(EShLanguage::EShLangCompute);
-
-            printf("intermediate = %p\n", intermediate);
 
             std::vector<uint32_t> spirv;
 
@@ -79,11 +125,7 @@ extern "C"
             spv_options.validate = true;
             spv_options.generateDebugInfo = true;
 
-            printf("before spv\n");
-
             glslang::GlslangToSpv(*intermediate, spirv, &logger, &spv_options);
-
-            printf("generated %d opcodes\n", spirv.size());
 
             *spv_size = spirv.size() * 4;
             spv = reinterpret_cast<uint8_t *>(allocator(*spv_size));
@@ -103,7 +145,7 @@ extern "C"
 
         if (!allocator)
         {
-            allocator = malloc;
+            allocator = compushady_khr_malloc;
         }
 
         try
@@ -132,7 +174,7 @@ extern "C"
         return hlsl;
     }
 
-    COMPUSHADY_KHR_API const uint8_t *compushady_khr_spv_to_msl(const uint8_t *spirv, const size_t spirv_size, void *(*allocator)(const size_t), size_t *msl_size)
+    COMPUSHADY_KHR_API const uint8_t *compushady_khr_spv_to_msl(const uint8_t *spirv, const size_t spirv_size, void *(*allocator)(const size_t), size_t *msl_size, uint32_t *x, uint32_t *y, uint32_t *z)
     {
         uint8_t *msl = nullptr;
         *msl_size = 0;
@@ -140,13 +182,17 @@ extern "C"
 
         if (!allocator)
         {
-            allocator = malloc;
+            allocator = compushady_khr_malloc;
         }
 
         try
         {
             spirv_cross::CompilerMSL msl_compiler(reinterpret_cast<const uint32_t *>(spirv), spirv_size / sizeof(uint32_t));
+            *x = msl_compiler.get_execution_mode_argument(spv::ExecutionMode::ExecutionModeLocalSize, 0);
+            *y = msl_compiler.get_execution_mode_argument(spv::ExecutionMode::ExecutionModeLocalSize, 1);
+            *z = msl_compiler.get_execution_mode_argument(spv::ExecutionMode::ExecutionModeLocalSize, 2);
             spirv_cross::CompilerMSL::Options options;
+            options.set_msl_version(2, 3);
             msl_compiler.set_msl_options(options);
             msl_code = msl_compiler.compile();
         }
@@ -175,7 +221,7 @@ extern "C"
 
         if (!allocator)
         {
-            allocator = malloc;
+            allocator = compushady_khr_malloc;
         }
 
         const uint32_t *spirv_ptr = reinterpret_cast<const uint32_t *>(spirv);
